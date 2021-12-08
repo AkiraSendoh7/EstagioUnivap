e4aa59390dd4d309a583ff7684cb286bc92e3a4b
Murilo Santos Lopes Carvalho <70426766+AkiraSendoh7@users.noreply.github.com>
2021-12-08 20:57:38 -0300

    Update banco.php

--- assets/php/conexao/banco.php
+++ assets/php/conexao/banco.php
@@ -1,8 +1,22 @@
 <?php
-    $servername = "us-cdbr-east-04.cleardb.com";
-    $username = "b8f85f9d421dfb";
-    $password = "ac8d19ff";
-    $database = 'heroku_9a4d01b9f8d28e2';
+    // $servername = "us-cdbr-east-04.cleardb.com";
+    // $username = "b8f85f9d421dfb";
+    // $password = "ac8d19ff";
+    // $database = 'heroku_9a4d01b9f8d28e2';
+
+    // // Create connection
+    // $con = new mysqli($servername, $username, $password, $database);
+
+    // // Check connection
+    // if ($con->connect_error) 
+    // {
+    //     die("Connection failed: " . $con->connect_error);
+    // }
+
+    $servername = "sql308.epizy.com";
+    $username = "epiz_30549063";
+    $password = "vHs0F00zYIQSLDk";
+    $database = 'epiz_30549063_estagio';
 
     // Create connection
     $con = new mysqli($servername, $username, $password, $database);
@@ -12,3 +26,4 @@
     {
         die("Connection failed: " . $con->connect_error);
     }
+
