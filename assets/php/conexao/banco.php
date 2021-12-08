37d685289975374c0616aee032d5d01dcb729844
Murilo Santos Lopes Carvalho <70426766+AkiraSendoh7@users.noreply.github.com>
2021-12-08 20:29:58 -0300

    Update banco.php

--- assets/php/conexao/banco.php
+++ assets/php/conexao/banco.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = 'estagio';
+    $servername = "us-cdbr-east-04.cleardb.com";
+    $username = "b8f85f9d421dfb";
+    $password = "ac8d19ff";
+    $database = 'heroku_9a4d01b9f8d28e2';
 
     // Create connection
     $con = new mysqli($servername, $username, $password, $database);
